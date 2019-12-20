d2be6feffdc3d4d86f98be52308088bd5e1f3ca8
Austin Johnson <technicallyaustin@gmail.com>
2019-12-20 00:05:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,21 @@ require 'pry'
 katz_deli = []
 
 def line(katz_deli)
-  counter = 0
-  place = 1
   if katz_deli.count == 0 
     puts "The line is currently empty."
   
   elsif katz_deli.count > 0 
+  counter = 0 
+  place = 1
+  formatted_arr = []
     while counter < katz_deli.count do
-      deli_line = katz_deli.join("#{place}.")
-      puts "The line is currently: #{deli_line}"
+      name_and_place =["#{place}. #{katz_deli[counter]}"]
+      formatted_arr << name_and_place
+      counter += 1
+      place += 1
+    end
+    new_str = name_and_place.join(" ")
+    puts "The line is currently: #{new_str}"
     end
   end
 end
